1e342a2b61d245aafb5d3c8ffccfea9fff912e03
NDERI007 <tnderi373@gmail.com>
2025-08-07 20:54:13 +0300

    views workaround failed, approach it too outdated

--- __manifest__.py
+++ __manifest__.py
@@ -11,17 +11,20 @@
     'depends': ['point_of_sale', 'pos_restaurant', 'stock'],
 
     'data': [
+        'views/JS_assets.xml',
         'views/payment_method.xml',
     ],
 
     'assets': {
-        'web.assests_common': [
+        'point_of_sale.assets_pos': [
             'fcl_module/static/src/js/model.js',
             'fcl_module/static/src/js/paystack_test.js',
         ]
     },
+    'web.assets_qweb': [
+        'fcl_pos_module/views/JS_assets.xml',
+    ],
 
     'installable': True,
     'application': True,
-    'license': 'LGPL-3',
 }
